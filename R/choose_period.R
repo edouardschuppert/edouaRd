39e8dc130d4e478c45cd16009313ef8a65ad2ded
edouardschuppert <edouard.schuppert@gmail.com>
2018-05-28 16:25:46 +0200

    Adding choose_period function

---
+++ R/choose_period.R
@@ -0,0 +1,25 @@
+#' choose_period
+#'
+#' Time Filter function
+#'
+#' @param df Your data frame
+#' @param colonne Your column
+#' @param startday The first day of the period you want to select
+#' @param endday The last day of the period you want to select
+#' @param starttime The start time of the period you want to select. Default set to 00:00:00
+#' @param andtime The end time of the period you want to select. Default set to 23:59:59
+#' @return Return a text, without any accent or links
+#' @export
+
+choose_period <- function(df, colonne, startday, endday, starttime = "00:00:00", endtime = "23:59:59"){
+
+  starttimestamp <- as.integer(lubridate::as_datetime(paste(startday, starttime)))
+  endtimestamp <- as.integer(lubridate::as_datetime(paste(endday, endtime)))
+
+  colonne <- rlang::enquo(colonne)
+
+  df %>%
+    dplyr::filter(!!colonne > starttimestamp &
+           !!colonne < endtimestamp)
+
+}
