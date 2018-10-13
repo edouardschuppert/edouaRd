8474774c519970af3c1ba64abcf30f8297e994d8
Edouard <edouard.schuppert@gmail.com>
2018-10-13 20:56:57 +0200

    sampler project

---
+++ R/sampler.R
@@ -0,0 +1,11 @@
+#' hashtagfrequency
+#'
+#' @param df Your data frame
+#' @param colonne Your column
+#' @param slice Allows you to keep only a small number of observations, starting from the first. Default set to NA
+#' @return Return the hashtag frequency of the column you chose
+#' @export
+
+# corpus.hasard <- donnees.import[sample(1:as.numeric(length(donnees.import$id)), 500),]
+
+# Fonction d'échantillonnage à faire
