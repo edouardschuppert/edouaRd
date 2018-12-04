05a42d2205984eab79e9383ad483edd7e89ecba7
edouardschuppert <edouard.schuppert@gmail.com>
2018-12-04 15:04:11 +0100

    hashtagsfrequency : use of ASCII

--- R/hashtagfrequency.R
+++ R/hashtagfrequency.R
@@ -1,5 +1,8 @@
 #' hashtagfrequency
 #'
+#' @import dplyr
+#' @import stringr
+#' @importFrom tidytext unnest_tokens
 #' @param df Your data frame
 #' @param colonne Your column
 #' @param slice Allows you to keep only a small number of observations, starting from the first. Default set to NA
@@ -13,21 +16,21 @@ hashtagfrequency <- function(df, colonne, slice = NA, original = FALSE) {
 
   # Processing
   df_new <- df %>%
-    dplyr::select(!!colonne) %>%
-    dplyr::mutate(colonne = povertext(!!colonne)) %>%
-    dplyr::mutate(hash = as.character(stringr::str_extract_all(colonne, "(?:(?:^|[[:space:]]+)|(?:[[:punct:]])?)#(?:[^[:blank:]]*|[^[:space:]]*)(?:(?:(?:[[:punct:]])?|[[:space:]])|$)"))) %>%
-    dplyr::mutate(hash = stringr::str_remove_all(hash, "\"")) %>%
-    dplyr::mutate(hash = stringr::str_remove_all(hash, rex::rex("c(" %or% ")" %or% "[[:punct:]]" %or% ","))) %>%
-    tidytext::unnest_tokens(words, hash) %>%
-    dplyr::filter(is.na(words) == FALSE) %>%
-    dplyr::mutate(words = stringr::str_remove_all(words, "[[:space:]]+")) %>%
-    dplyr::count(words) %>%
-    dplyr::arrange(desc(n)) %>%
-    dplyr::mutate(hashtags = paste0("#", words)) %>%
-    dplyr::select(hashtags, n)
+    select(!!colonne) %>%
+    mutate(colonne = povertext(!!colonne)) %>%
+    mutate(hash = as.character(str_extract_all(colonne, "(?:(?:^|[[:space:]]+)|(?:[[:punct:]])?)#(?:[^[:blank:]]*|[^[:space:]]*)(?:(?:(?:[[:punct:]])?|[[:space:]])|$)"))) %>%
+    mutate(hash = str_remove_all(hash, "\"")) %>%
+    mutate(hash = str_remove_all(hash, rex::rex("c(" %or% ")" %or% "[[:punct:]]" %or% ","))) %>%
+    unnest_tokens(words, hash) %>%
+    filter(is.na(words) == FALSE) %>%
+    mutate(words = str_remove_all(words, "[[:space:]]+")) %>%
+    count(words) %>%
+    arrange(desc(n)) %>%
+    mutate(hashtags = paste0("#", words)) %>%
+    select(hashtags, n)
 
   df_new <- df_new %>%
-    dplyr::filter(hashtags != "#n" &
+    filter(hashtags != "#n" &
                     hashtags != "#0" &
                     hashtags != "#u" &
                     hashtags != "#character")
@@ -36,7 +39,7 @@ hashtagfrequency <- function(df, colonne, slice = NA, original = FALSE) {
   if (is.na(slice) == FALSE) {
 
     df_new <- df_new %>%
-      dplyr::slice(1:slice)
+      slice(1:slice)
 
   }
 
@@ -49,15 +52,38 @@ hashtagfrequency <- function(df, colonne, slice = NA, original = FALSE) {
       temp_request <- df_new$hashtags[i]
 
       request <- df %>%
-        dplyr::filter(stringr::str_detect(povertext(!!colonne), temp_request)) %>%
-        dplyr::mutate(text = !!colonne,
-                      text =  stringr::str_replace_all(text, "(?:(?:(?:é|ê)|è)|ë)", "e"),
-                      text = stringr::str_replace_all(text, "(?:(?:à|â)|ä)", "a"),
-                      text = stringr::str_replace_all(text, "(?:(?:û|ù)|ü)","u"),
-                      text = stringr::str_replace_all(text, "(?:î|ï)", "i"),
-                      text = stringr::str_replace_all(text, "(?:ö|ô)", "o")) %>%
-        dplyr::mutate(xx = stringr::str_extract(text, stringr::regex(temp_request, ignore_case = TRUE))) %>%
-        dplyr::count(xx, sort = TRUE)
+        filter(str_detect(povertext(!!colonne), temp_request)) %>%
+        mutate(text = !!colonne,
+               text = str_replace_all(val, "\u00E0", "a"),
+               text = str_replace_all(val, "\u00E1", "a"),
+               text = str_replace_all(val, "\u00E2", "a"),
+               text = str_replace_all(val, "\u00E3", "a"),
+               text = str_replace_all(val, "\u00E4", "a"),
+               text = str_replace_all(val, "\u00E5", "a"),
+               text = str_replace_all(val, "\u00E9", "e"),
+               text = str_replace_all(val, "\u00E8", "e"),
+               text = str_replace_all(val, "\u00EA", "e"),
+               text = str_replace_all(val, "\u00EB", "e"),
+               text = str_replace_all(val, "\u00EC", "i"),
+               text = str_replace_all(val, "\u00ED", "i"),
+               text = str_replace_all(val, "\u00EE", "i"),
+               text = str_replace_all(val, "\u00EF", "i"),
+               text = str_replace_all(val, "\u00F2", "o"),
+               text = str_replace_all(val, "\u00F3", "o"),
+               text = str_replace_all(val, "\u00F4", "o"),
+               text = str_replace_all(val, "\u00F5", "o"),
+               text = str_replace_all(val, "\u00F6", "o"),
+               text = str_replace_all(val, "\u00F9", "u"),
+               text = str_replace_all(val, "\u00FA", "u"),
+               text = str_replace_all(val, "\u00FB", "u"),
+               text = str_replace_all(val, "\u00FC", "u"),
+               text = str_replace_all(val, "\u00FF", "y"),
+               text = str_replace_all(val, "\u00FD", "y"),
+               text = str_replace_all(val, "\u00E7", "c"),
+               text = str_replace_all(val, "\u00E6", "ae"),
+               text = str_replace_all(val, "\u00F1", "n")) %>%
+        mutate(xx = str_extract(text, regex(temp_request, ignore_case = TRUE))) %>%
+        count(xx, sort = TRUE)
 
       df_new$hashtags[i] <- request$xx[1]
 
