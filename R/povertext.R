e02cfde5e8bb634062b458cff780a5f4e3c81790
edouardschuppert <edouard.schuppert@gmail.com>
2018-12-04 15:03:56 +0100

    Povertext : use of ASCII

--- R/povertext.R
+++ R/povertext.R
@@ -1,20 +1,52 @@
 #' povertext
 #'
 #' Deplete the text value
-#'
+#' @importFrom stringr str_to_lower
+#' @importFrom stringr str_replace_all
 #' @param val Your data frame
 #' @return Return a text, without any accent or links
 #' @export
 
-povertext <- function(val){
+povertext <- function(val) {
 
-  val <- stringr::str_to_lower(val)
-  val <- stringr::str_remove_all(val, "http(?:s)?://[^[:space:]]*") # Links are deleted
-  val <- stringr::str_replace_all(val, "(?:(?:(?:é|ê)|è)|ë)", "e")
-  val <- stringr::str_replace_all(val, "(?:(?:à|â)|ä)", "a")
-  val <- stringr::str_replace_all(val, "(?:(?:û|ù)|ü)","u")
-  val <- stringr::str_replace_all(val, "(?:î|ï)", "i")
-  val <- stringr::str_replace_all(val, "(?:ö|ô)", "o")
+  val <- str_to_lower(val)
+
+  val <- str_remove_all(val, "http(?:s)?://[^[:space:]]*") # Links are deleted
+
+  val <- str_replace_all(val, "\u00E0", "a")
+  val <- str_replace_all(val, "\u00E1", "a")
+  val <- str_replace_all(val, "\u00E2", "a")
+  val <- str_replace_all(val, "\u00E3", "a")
+  val <- str_replace_all(val, "\u00E4", "a")
+  val <- str_replace_all(val, "\u00E5", "a")
+
+  val <- str_replace_all(val, "\u00E9", "e")
+  val <- str_replace_all(val, "\u00E8", "e")
+  val <- str_replace_all(val, "\u00EA", "e")
+  val <- str_replace_all(val, "\u00EB", "e")
+
+  val <- str_replace_all(val, "\u00EC", "i")
+  val <- str_replace_all(val, "\u00ED", "i")
+  val <- str_replace_all(val, "\u00EE", "i")
+  val <- str_replace_all(val, "\u00EF", "i")
+
+  val <- str_replace_all(val, "\u00F2", "o")
+  val <- str_replace_all(val, "\u00F3", "o")
+  val <- str_replace_all(val, "\u00F4", "o")
+  val <- str_replace_all(val, "\u00F5", "o")
+  val <- str_replace_all(val, "\u00F6", "o")
+
+  val <- str_replace_all(val, "\u00F9", "u")
+  val <- str_replace_all(val, "\u00FA", "u")
+  val <- str_replace_all(val, "\u00FB", "u")
+  val <- str_replace_all(val, "\u00FC", "u")
+
+  val <- str_replace_all(val, "\u00FF", "y")
+  val <- str_replace_all(val, "\u00FD", "y")
+
+  val <- str_replace_all(val, "\u00E7", "c")
+  val <- str_replace_all(val, "\u00E6", "ae")
+  val <- str_replace_all(val, "\u00F1", "n")
 
   val
 
